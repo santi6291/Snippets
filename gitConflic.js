61861b62d92c80c8c7a1703c844a119a79fe18cb
Santiago Sosa <ssosa@santiagojsosa.com>
2015-11-11 14:38:51 -0500

    Create gitConflic.js

---
+++ gitConflic.js
@@ -0,0 +1,14 @@
+# Solve git conflic
+Purpose: will remove conflic comments and leave whatever is in HEAD
+
+```
+var fs = require('fs');
+var fileName = '/Users/santiago/Sites/xaxis/Xaxis-2.0/assets/scss/_style.scss';
+fs.readFile(fileName, 'utf-8', function(err, data){
+	var cleanData = data.replace( /(<<<<<<< HEAD)|(\=======[\w\W]*?\>>>>>>>.*)/g, '' );
+
+	fs.writeFile(fileName, cleanData, 'utf8', function (err) {
+		if (err) return console.log(err);
+	});
+});
+```
